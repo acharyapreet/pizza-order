c51a4535f574009d663b7d2baf9fa6c7a8e986d1
Preet Acharya <acharyapreet2@gmail.com>
2025-01-08 19:16:44 +0530

    tested userSchema

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
+const User = require('./schema/userSchema');
 
 const app = express();
 
@@ -18,4 +19,14 @@ app.post('/ping',(req, res) => {
 app.listen(ServerConfig.PORT, async () => {
     await connectDB();
     console.log(`server started at port ${ServerConfig.PORT}....`);
+
+    // const newUser = await User.create({
+    //     email : 'a@b.com',
+    //     password : "123456",
+    //     firstName : "Abhay",
+    //     LastName : "Kumar",
+    //     mobileNumber : "1234567890"
+    // });
+
+    // console.log(newUser);
 });
