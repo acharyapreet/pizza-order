c01a116ebda75916e72e7d55a9df6f0d0a2a9b6b
Preet Acharya <acharyapreet2@gmail.com>
2026-01-06 15:27:39 +0530

    added database

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 module.exports = {
     PORT : process.env.PORT ,
-    DB_URL : 'mongodb://localhost/basics',
+    DB_URL : process.env.DB_URL,
     JWT_SECRET : process.env.JWT_SECRET,
     JWT_EXPIRY : process.env.JWT_EXPIRY,
     CLOUDINARY_CLOUD_NAME : process.env.CLOUDINARY_CLOUD_NAME,
